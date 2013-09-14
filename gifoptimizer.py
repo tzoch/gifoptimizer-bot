175b1ae9e67c4d551f41d216bc7ef22826fdb929
Travis <tzoch2008@gmail.com>
2013-09-14 03:22:19 -0400

    Started a method of managing temporary files.

--- gifoptimizer.py
+++ gifoptimizer.py
@@ -5,19 +5,35 @@ import os
 
 import PythonMagick
 
-target_im = 'example_images/gta_original.gif'
+target_im = 'example_images/plane_original.gif'
 #im = PythonMagick.Image(target_im)
 
 extract_frames = ['convert', '-coalesce', '+adjoin', 
                    target_im, 'tmp/%03d.png']
 
-recombine_frames = ['convert', '-resize', '75%', '-fuzz', '2%', '-loop',
-                    '0', 'tmp/*.png', '-layers', 'OptimizePlus', '-layers', 
-                    'OptimizeTransparency', 'tmp.gif']
+recombine_frames = ['convert', '-resize', '75%', '-fuzz', '2%', '-delay', '4',
+                    '-loop', '0', 'tmp/*.png', '-layers', 'OptimizePlus',
+                    '-layers', 'OptimizeTransparency', 'tmp.gif']
 
 reduce_colors = ['gifsicle', '-O3', '--colors', '128', '-d4', 'tmp.gif', 
-                 '-o', 'final.gif']
+                 '-o', 'final2.gif']
 
-subprocess.call(extract_frames)
-subprocess.call(recombine_frames)
-subprocess.call(reduce_colors)
+#subprocess.call(extract_frames)
+#subprocess.call(recombine_frames)
+#subprocess.call(reduce_colors)
+
+def removeTempFiles(directory):
+    files_to_delete = os.listdir(directory)
+    if not files_to_delete:
+        print 'No temporary files to delete!'
+        return
+    
+    for image in files_to_delete:
+        os.remove(directory + '/' + image)
+
+    print 'All temporary files deleted!'    
+    return
+
+removeTempFiles('tmp')
+
+
