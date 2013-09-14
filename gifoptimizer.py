4ec954d9be86827c52920f4651c232631cc1a8e3
Travis <tzoch2008@gmail.com>
2013-09-14 01:16:20 -0400

    Testing terminal commands.

--- gifoptimizer.py
+++ gifoptimizer.py
@@ -1,13 +1,23 @@
 #! /usr/bin/python
 
 import subprocess
+import os
 
 import PythonMagick
 
-im = PythonMagick.Image("example_images/plane_original.gif")
+target_im = 'example_images/gta_original.gif'
+#im = PythonMagick.Image(target_im)
 
-print im.fileName()
-print im.magick()
-print im.size().width()
-print im.size().height()
-print dir(im.format())
+extract_frames = ['convert', '-coalesce', '+adjoin', 
+                   target_im, 'tmp/%03d.png']
+
+recombine_frames = ['convert', '-resize', '75%', '-fuzz', '2%', '-loop',
+                    '0', 'tmp/*.png', '-layers', 'OptimizePlus', '-layers', 
+                    'OptimizeTransparency', 'tmp.gif']
+
+reduce_colors = ['gifsicle', '-O3', '--colors', '128', '-d4', 'tmp.gif', 
+                 '-o', 'final.gif']
+
+subprocess.call(extract_frames)
+subprocess.call(recombine_frames)
+subprocess.call(reduce_colors)
