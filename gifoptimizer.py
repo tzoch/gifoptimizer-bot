b480bb08f513359bda18d5121732a7b1fa19a1db
travis <travis@travis-VirtualBox.(none)>
2013-09-13 22:58:30 -0400

    Testing PythonMagick

--- gifoptimizer.py
+++ gifoptimizer.py
@@ -1,6 +1,13 @@
 #! /usr/bin/python
 
 import subprocess
+
 import PythonMagick
 
+im = PythonMagick.Image("example_images/plane_original.gif")
 
+print im.fileName()
+print im.magick()
+print im.size().width()
+print im.size().height()
+print dir(im.format())
